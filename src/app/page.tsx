7e9ec13ef6af009f10a5aec70eb90a62dee29a4f
Gabriel <knuckles4x@gmail.com>
2025-09-20 01:40:02 -0300

    feat: shake

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-center">
         {showConfetti && <Confetti />}
-        <div className="flex flex-col items-center">
+        <div className={`flex flex-col items-center ${showConfetti ? 'shake' : ''}`}>
           <Image
             src={imageSrc}
             alt="Next.js logo"
